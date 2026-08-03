d9bfb0df7af01b87b1bfaa9bff4bc995a8af011f
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2026-08-03 15:45:46 -0500

    Added backward compatibiliy code in CsRegs::configCsr: If MSTATUS.XS is configured to be writable/pokable, so will VSSTATUS.XS. XS should not be writable in spec compliant configurations.

--- CsRegs.cpp
+++ CsRegs.cpp
@@ -4370,6 +4370,19 @@ CsRegs<URV>::configCsr(CsrNumber csrNum, bool implemented, URV resetValue,
       ssf = MstatusFields<URV>(sstatus.getPokeMask());
       ssf.bits_.XS = msf.bits_.XS;
       sstatus.setPokeMask(ssf.value_);
+
+      // Same for VSSTATUS.XS write mask
+      auto& vsstatus = regs_.at(size_t(CsrNumber::VSSTATUS));
+      msf = MstatusFields<URV>(csr.getWriteMask());
+      MstatusFields<URV> vssf(vsstatus.getWriteMask());
+      vssf.bits_.XS = msf.bits_.XS;
+      vsstatus.setWriteMask(vssf.value_);
+
+      // Same for VSSTATUS.XS poke mask
+      msf = MstatusFields<URV>(csr.getPokeMask());
+      vssf = MstatusFields<URV>(vsstatus.getPokeMask());
+      vssf.bits_.XS = msf.bits_.XS;
+      vsstatus.setPokeMask(vssf.value_);
     }
 
   return true;
@@ -5243,9 +5256,11 @@ CsRegs<URV>::defineHypervisorRegs()
   //           D E        S W V X U P S  S  P  S  P P B P P I E I I
   //             S        R   M R M R       P     P I E I I E S E E
   //                                V               E   E E
-  mask     = 0b0'00000000'0'0'0'1'1'0'11'11'00'11'1'0'0'1'0'0'0'1'0;
+  mask     = 0b0'00000000'0'0'0'1'1'0'00'11'00'11'1'0'0'1'0'0'0'1'0;
   URV val  = 0b0'00000000'0'0'0'0'0'0'00'00'00'00'0'0'0'0'0'0'0'0'0;
   pokeMask = mask | (URV(1) << (sizeof(URV)*8 - 1));  // Make SD pokable.
+  pokeMask |= URV(3) << 15; // Make XS pokable.
+
   if (not rv32_)
     {
       val |= uint64_t(0b10) << 32;  // Value of UXL: uxlen=64
