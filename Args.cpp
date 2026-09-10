90cf05c42c3432f8d10d7a8cd262337a0851f968
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2026-09-10 12:46:54 -0500

    Upped version.

--- Args.cpp
+++ Args.cpp
@@ -25,7 +25,7 @@ void
 printVersion()
 {
   unsigned version = 1;
-  unsigned subversion = 862;
+  unsigned subversion = 863;
   std::cout << "Version " << version << "." << subversion << " compiled on "
 	    << __DATE__ << " at " << __TIME__ << '\n';
 #ifdef GIT_SHA
