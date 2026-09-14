d2e55c62b9494eff9797327793767b0bc60eba24
Joseph Rahmeh <jrahmeh@tenstorrent.com>
2026-09-14 11:13:38 -0500

    Fixed bug in cbo.clean/flush/inval/zero: We were using SENVCFG fields even when the S extension was not enabled. We now gate such use with isRvs.

--- cbo.cpp
+++ cbo.cpp
@@ -142,21 +142,23 @@ Hart<URV>::execCbo_clean(const DecodedInst* di)
   SenvcfgFields<uint64_t> senvf(isRvs()? csRegs_.read64(CN::SENVCFG) : 0);
   HenvcfgFields<uint64_t> henvf(isRvh()? csRegs_.read64(CN::HENVCFG) : 0);
 
+  bool is_u = pm == PM::User and not virtMode_;
+  bool is_vu = pm == PM::User and virtMode_;
+  bool is_vs = pm == PM::Supervisor and virtMode_;
+
+  // Spec pseudo-code does not gate SENNVCFG with S extension being implemented, we do
+  // with the isRvs() below. Spec should be fixed.
   if ( (pm != PM::Machine and not menvf.bits_.CBCFE) or
-       (not virtMode_ and pm == PM::User and not senvf.bits_.CBCFE) )
+       (isRvs() and is_u and not senvf.bits_.CBCFE) )
     {
       illegalInst(di);
       return;
     }
-
-  if (virtMode_)
+  else if ( (is_vs and not henvf.bits_.CBCFE) or
+            (is_vu and not (henvf.bits_.CBCFE and senvf.bits_.CBCFE)) )
     {
-      if ( (pm == PM::Supervisor and not henvf.bits_.CBCFE) or
-	   (pm == PM::User and not (henvf.bits_.CBCFE and senvf.bits_.CBCFE)) )
-	{
-	  virtualInst(di);
-	  return;
-	}
+      virtualInst(di);
+      return;
     }
 
   uint64_t virtAddr = intRegs_.read(di->op0());
@@ -215,21 +217,23 @@ Hart<URV>::execCbo_flush(const DecodedInst* di)
   SenvcfgFields<uint64_t> senvf(isRvs()? csRegs_.read64(CN::SENVCFG) : 0);
   HenvcfgFields<uint64_t> henvf(isRvh()? csRegs_.read64(CN::HENVCFG) : 0);
 
+  bool is_u = pm == PM::User and not virtMode_;
+  bool is_vu = pm == PM::User and virtMode_;
+  bool is_vs = pm == PM::Supervisor and virtMode_;
+
+  // Spec pseudo-code does not gate SENNVCFG with S extension being implemented, we do
+  // with the isRvs() below. Spec should be fixed.
   if ( (pm != PM::Machine and not menvf.bits_.CBCFE) or
-       (not virtMode_ and pm == PM::User and not senvf.bits_.CBCFE) )
+       (isRvs() and is_u and not senvf.bits_.CBCFE) )
     {
       illegalInst(di);
       return;
     }
-
-  if (virtMode_)
+  else if ( (is_vs and not henvf.bits_.CBCFE) or
+            (is_vu and not (henvf.bits_.CBCFE and senvf.bits_.CBCFE)) )
     {
-      if ( (pm == PM::Supervisor and not henvf.bits_.CBCFE) or
-	   (pm == PM::User and not (henvf.bits_.CBCFE and senvf.bits_.CBCFE)) )
-	{
-	  virtualInst(di);
-	  return;
-	}
+      virtualInst(di);
+      return;
     }
 
   uint64_t virtAddr = intRegs_.read(di->op0());
@@ -285,21 +289,23 @@ Hart<URV>::execCbo_inval(const DecodedInst* di)
   SenvcfgFields<uint64_t> senvf(isRvs()? csRegs_.read64(CN::SENVCFG) : 0);
   HenvcfgFields<uint64_t> henvf(isRvh()? csRegs_.read64(CN::HENVCFG) : 0);
 
+  bool is_u = pm == PM::User and not virtMode_;
+  bool is_vu = pm == PM::User and virtMode_;
+  bool is_vs = pm == PM::Supervisor and virtMode_;
+
+  // Spec pseudo-code does not gate SENNVCFG with S extension being implemented, we do:
+  // the isRvs() below. Spec should be fixed.
   if ( (pm != PM::Machine and menvf.bits_.CBIE == 0) or
-       (not virtMode_ and pm == PM::User and senvf.bits_.CBIE == 0) )
+       (isRvs() and is_u and senvf.bits_.CBIE == 0) )
     {
       illegalInst(di);
       return;
     }
-
-  if (virtMode_)
+  else if ( (is_vs and henvf.bits_.CBIE == 0) or
+            (is_vu and (henvf.bits_.CBIE == 0 or senvf.bits_.CBIE == 0)) )
     {
-      if ( (pm == PM::Supervisor and henvf.bits_.CBIE == 0) or
-	   (pm == PM::User and (henvf.bits_.CBIE == 0 or senvf.bits_.CBIE == 0)) )
-	{
-	  virtualInst(di);
-	  return;
-	}
+      virtualInst(di);
+      return;
     }
 
   bool isZero = false;
@@ -362,21 +368,23 @@ Hart<URV>::execCbo_zero(const DecodedInst* di)
   SenvcfgFields<uint64_t> senvf(isRvs()? csRegs_.read64(CN::SENVCFG) : 0);
   HenvcfgFields<uint64_t> henvf(isRvh()? csRegs_.read64(CN::HENVCFG) : 0);
 
+  bool is_u = pm == PM::User and not virtMode_;
+  bool is_vu = pm == PM::User and virtMode_;
+  bool is_vs = pm == PM::Supervisor and virtMode_;
+
+  // Spec pseudo-code does not gate SENNVCFG with S extension being implemented, we do
+  // with the isRvs() below. Spec should be fixed.
   if ( (pm != PM::Machine and not menvf.bits_.CBZE) or
-       (not virtMode_ and pm == PM::User and not senvf.bits_.CBZE) )
+       (isRvs() and is_u and not senvf.bits_.CBZE) )
     {
       illegalInst(di);
       return;
     }
-
-  if (virtMode_)
+  else if ( (is_vs and not henvf.bits_.CBZE) or
+            (is_vu and not (henvf.bits_.CBZE and senvf.bits_.CBZE)) )
     {
-      if ( (pm == PM::Supervisor and not henvf.bits_.CBZE) or
-	   (pm == PM::User and not (henvf.bits_.CBZE and senvf.bits_.CBZE)) )
-	{
-	  virtualInst(di);
-	  return;
-	}
+      virtualInst(di);
+      return;
     }
 
   // Translate virtual addr and check for exception.
